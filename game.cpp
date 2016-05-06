18afafe60fba7b6212c34177a7b24efd485c436f
Andrzej Żukowski <dftruf@gmail.com>
2016-05-06 16:34:37 +0200

    Added files via upload

--- game.cpp
+++ game.cpp
@@ -116,12 +116,12 @@ void initGame() {
   
   Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ]->addPlaneMatrix();
   Game.res->tileSequences[ 0 ].actualVO = Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ]->putSeqOnMatrix( 3, &Game.res->tileSequences[ 0 ], 0, 0 );
-  Game.res->tileSequences[ 0 ].win = Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ];
+  //Game.res->tileSequences[ 0 ].actualVO->win = Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ];
   //Game.res->tileSequences[ 0 ].nextFrame = -1;
   Game.res->tileSequences[ 1 ].actualVO = Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ]->putSeqOnMatrix( 3, &Game.res->tileSequences[ 1 ], 64, 64 );
-  Game.res->tileSequences[ 1 ].win = Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ];
-  //Game.res->tileSequences[ 1 ].looped = true;
-  //Game.res->tileSequences[ 0 ].looped = true;
+  //Game.res->tileSequences[ 1 ].actualVO->win = Game.screens[ SCREEN_GAME ].windows[ WINDOW_LEVEL ];
+  Game.res->tileSequences[ 1 ].looped = true;
+  Game.res->tileSequences[ 0 ].looped = true;
   
   Game.screens[ SCREEN_GAME ].redraw();
   Game.res->tileSequences[ 0 ].start();
